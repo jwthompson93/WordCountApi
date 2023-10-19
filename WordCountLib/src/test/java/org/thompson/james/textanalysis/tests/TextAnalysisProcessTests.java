b473b466e088497c2dadb2b3f6eedc841ea98595
jwthompson93 <shadow17993@gmail.com>
2023-10-19 19:54:24 +0100

    Cleaned up files and unused packages. Changed JsonParser to use the up to date static version

--- WordCountLib/src/test/java/org/thompson/james/textanalysis/tests/TextAnalysisProcessTests.java
+++ WordCountLib/src/test/java/org/thompson/james/textanalysis/tests/TextAnalysisProcessTests.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package org.thompson.james.textanalysis.tests;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 import java.io.IOException;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.thompson.james.file.TextFileReader;
 import org.thompson.james.process.IProcess;
 import org.thompson.james.process.TextAnalysisProcess;
 import org.thompson.james.textanalysis.object.TextAnalysisResult;
@@ -23,11 +17,9 @@ import org.thompson.james.textanalysis.object.TextAnalysisResult;
 public class TextAnalysisProcessTests {
     
     private Gson gson;
-    private JsonParser jsonParser;
     
     public TextAnalysisProcessTests() {
         gson = new Gson();
-        jsonParser = new JsonParser();
     }
     
     @ParameterizedTest
@@ -35,7 +27,8 @@ public class TextAnalysisProcessTests {
     void Test_ProcessString(TextAnalysisResult input) throws IOException {
         IProcess<String> textAnalysisProcess = new TextAnalysisProcess();
         String JsonResult = textAnalysisProcess.process(input.getSentence());
+        System.out.println(JsonResult);
         
-        assertEquals(jsonParser.parse(JsonResult), jsonParser.parse(gson.toJson(input)));
+        assertEquals(JsonParser.parseString(JsonResult), JsonParser.parseString(gson.toJson(input)));
     }
 }
