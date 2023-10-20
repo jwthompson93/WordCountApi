c7f844a310ca121040e7d494ed5a8042d80fd60b
jwthompson93 <shadow17993@gmail.com>
2023-10-20 11:11:44 +0100

    Implemented the TextAnalysisController class with the API method to Process File

---
+++ WordCountApi/src/main/java/com/thompson/james/wordcountapi/controller/TextAnalysisController.java
@@ -0,0 +1,41 @@
+package com.thompson.james.wordcountapi.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.thompson.james.file.TextFileReader;
+import org.thompson.james.process.TextAnalysisProcess;
+
+/**
+ *
+ * @author James Thompson
+ */
+@RestController
+@RequestMapping("/api/textanalysis")
+public class TextAnalysisController {
+    
+    private TextAnalysisProcess textAnalysisProcess;
+    
+    public TextAnalysisController() {
+        textAnalysisProcess = new TextAnalysisProcess();
+    }
+    
+    @GetMapping()
+    public String ping() {
+        return "Pong!";
+    }
+    
+    @PostMapping(path = "/process", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> ProcessFile(@RequestParam("file") MultipartFile file) throws IOException {
+        TextFileReader fileReader = new TextFileReader();
+        String text = fileReader.getTextFromFile(file.getInputStream());
+        
+        String responseJson = textAnalysisProcess.process(text);
+        return ResponseEntity.ok(responseJson);
+    }
+}
